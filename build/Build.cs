06b301fc414fcd5ddc1147bf9e3b207025e5959c
Matthias Koch <ithrowexceptions@gmail.com>
2021-04-06 15:36:34 +0200

    Add Compile target

--- build/Build.cs
+++ build/Build.cs
@@ -14,4 +14,25 @@ partial class Build
     string PluginsFile => RootDirectory / "plugins.yml";
     IEnumerable<Plugin> Plugins => SerializationTasks.YamlDeserializeFromFile<List<Plugin>>(PluginsFile);
 
+    Target Compile => _ => _
+        .DependsOn(CheckoutExternalRepositories)
+        .Produces(ExternalRepositoriesDirectory / "*" / "output" / "*.zip")
+        .Produces(ExternalRepositoriesDirectory / "*" / "output" / "*.nupkg")
+        .Executes(() =>
+        {
+            foreach (var plugin in Plugins.Where(x => !x.name.StartsWith("_")))
+            {
+                TeamCity.Instance.SetBuildStatus($"{plugin.name}:", prepend: true);
+                using (Logger.Block(plugin.name))
+                {
+                    DotNetRun(_ => _
+                        .SetProjectFile(ReSharperRiderUpdateProjectFile)
+                        .SetProcessWorkingDirectory(GetPluginDirectory(plugin))
+                        .SetApplicationArguments("compile --root --no-logo"));
+                }
+            }
+        });
+
+    AbsolutePath ReSharperRiderUpdateProjectFile
+        => ExternalRepositoriesDirectory / "_ReSharperRiderUpdate" / "src" / "ReSharperRiderUpdate.csproj";
 }
